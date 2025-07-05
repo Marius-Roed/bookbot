40951c44434c2fe14b5a39e853c5ad2235f3d7ac
Marius-Roed <mwroed@gmail.com>
2025-07-05 11:48:40 +0200

    feat: sys argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_chars
 
@@ -7,7 +8,10 @@ def sort_num(item):
 
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
 
         print("============ BOOKBOT ============")
@@ -20,8 +24,7 @@ def main():
         chars = count_chars(file_contents)
         chars.sort(reverse=True, key=sort_num)
         for char in chars:
-            if char['char'].isalpha():
-                print(f"{char['char']}: {char['num']}")
+            print(f"{char['char']}: {char['num']}")
 
         print("============= END ===============")
 
